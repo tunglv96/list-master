1230feef0f0516eef0fe67022156313c6d23f6aa
Văn Tùng <lvtungdn43@gmail.com>
2019-08-26 10:20:28 +0700

    four committ

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      elmTaskMenu: false
+      elmTaskMenu: true
     }
   }
 
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -6,7 +6,7 @@ class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      elmTaskMenu: true
+      elmTaskMenu: false
     }
   }
   toggleMenu =   () => {
