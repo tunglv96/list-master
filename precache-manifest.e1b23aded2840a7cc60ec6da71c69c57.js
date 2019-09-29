c7a592feccb0a75758ec3e892de1322f88d407d0
Van Tung <lvtungdn43@gmail.com>
2019-09-29 13:39:25 +0700

    Updates

---
+++ precache-manifest.e1b23aded2840a7cc60ec6da71c69c57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374840dea8c549cfab69c76d6a64a49a",
+    "url": "/tunglv96/list-master/index.html"
+  },
+  {
+    "revision": "27388334c93fbd712670",
+    "url": "/tunglv96/list-master/static/css/main.797eaf0b.chunk.css"
+  },
+  {
+    "revision": "39505325971c08861456",
+    "url": "/tunglv96/list-master/static/js/2.32daf8ff.chunk.js"
+  },
+  {
+    "revision": "27388334c93fbd712670",
+    "url": "/tunglv96/list-master/static/js/main.38a0bbe7.chunk.js"
+  },
+  {
+    "revision": "67f2004e6877b66c587a",
+    "url": "/tunglv96/list-master/static/js/runtime~main.8aafab8c.js"
+  }
+]);
